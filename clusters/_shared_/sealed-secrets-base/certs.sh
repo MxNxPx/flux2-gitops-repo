18c5f3e222a07d35b68c19bfdb307fd9a53296ff
michael <mxnxpx@gmail.com>
2022-08-31 20:10:36 -0400

    removing some stuff

--- clusters/_shared_/sealed-secrets-base/certs.sh
+++
@@ -1,5 +0,0 @@
-export PRIVATEKEY="tls.key"
-export PUBLICKEY="tls.crt"
-export SECRETNAME="mycustomkeys"
-
-openssl req -x509 -nodes -newkey rsa:4096 -keyout "$PRIVATEKEY" -out "$PUBLICKEY" -subj "/CN=sealed-secret/O=sealed-secret"
